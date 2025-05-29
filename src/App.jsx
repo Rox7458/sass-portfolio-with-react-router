ae3630cb0e3e37b5615372796ac7bfb0a362125e
Rox7458 <jagaaoguz.7000@gmail.com>
2025-05-29 14:48:46 +0300

    installing on the structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import Router from "./router/Router";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
-}
+const App = () => {
+  return (
+    <div>
+      <Router />
+    </div>
+  );
+};
 
 export default App;
